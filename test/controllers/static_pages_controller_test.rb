9d81c75fae1a4eca26867950e32b2f5b3383ed9f
curtis nickel <curtis.h.nickel@berkeley.edu>
2014-11-20 22:20:42 +0000

    added some help tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,10 +4,12 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "h1", "DemoApp"
   end
 
   test "should get help" do
     get :help
+    assert_select "h1", "Help"
     assert_response :success
   end
 
