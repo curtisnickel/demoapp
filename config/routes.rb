ac6d434141ff5909af39bc6f1d229488f0cfbb53
curtis nickel <curtis.h.nickel@berkeley.edu>
2014-11-21 00:54:22 +0000

    Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  match 'help', to: 'static_pages#help', via 'get'
+  match 'help', to: 'static_pages#help', via: 'get'
 
   resources :users
 
